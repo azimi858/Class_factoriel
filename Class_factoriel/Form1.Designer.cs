b825448d61596098955b24fef89ac2629f5fd46c
MOHAMMADMAHDI AZIMIESFAHANI <TP017204@mail.apu.edu.my>
2021-05-04 04:24:56 +0430

    v.1

--- Class_factoriel/Form1.Designer.cs
+++ Class_factoriel/Form1.Designer.cs
@@ -28,13 +28,197 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button_factoriel = new System.Windows.Forms.Button();
+			this.button_7 = new System.Windows.Forms.Button();
+			this.button_8 = new System.Windows.Forms.Button();
+			this.button_9 = new System.Windows.Forms.Button();
+			this.button_4 = new System.Windows.Forms.Button();
+			this.button_5 = new System.Windows.Forms.Button();
+			this.button_6 = new System.Windows.Forms.Button();
+			this.button_1 = new System.Windows.Forms.Button();
+			this.button_2 = new System.Windows.Forms.Button();
+			this.button_3 = new System.Windows.Forms.Button();
+			this.button_0 = new System.Windows.Forms.Button();
+			this.button_clear = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// button_factoriel
+			// 
+			this.button_factoriel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button_factoriel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_factoriel.ForeColor = System.Drawing.Color.White;
+			this.button_factoriel.Location = new System.Drawing.Point(207, 79);
+			this.button_factoriel.Name = "button_factoriel";
+			this.button_factoriel.Size = new System.Drawing.Size(148, 56);
+			this.button_factoriel.TabIndex = 0;
+			this.button_factoriel.Text = "Factoriel";
+			this.button_factoriel.UseVisualStyleBackColor = false;
+			this.button_factoriel.Click += new System.EventHandler(this.button_factoriel_Click);
+			// 
+			// button_7
+			// 
+			this.button_7.Location = new System.Drawing.Point(12, 79);
+			this.button_7.Name = "button_7";
+			this.button_7.Size = new System.Drawing.Size(59, 56);
+			this.button_7.TabIndex = 3;
+			this.button_7.Text = "7";
+			this.button_7.UseVisualStyleBackColor = true;
+			this.button_7.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_8
+			// 
+			this.button_8.Location = new System.Drawing.Point(77, 79);
+			this.button_8.Name = "button_8";
+			this.button_8.Size = new System.Drawing.Size(59, 56);
+			this.button_8.TabIndex = 4;
+			this.button_8.Text = "8";
+			this.button_8.UseVisualStyleBackColor = true;
+			this.button_8.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_9
+			// 
+			this.button_9.Location = new System.Drawing.Point(142, 79);
+			this.button_9.Name = "button_9";
+			this.button_9.Size = new System.Drawing.Size(59, 56);
+			this.button_9.TabIndex = 5;
+			this.button_9.Text = "9";
+			this.button_9.UseVisualStyleBackColor = true;
+			this.button_9.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_4
+			// 
+			this.button_4.Location = new System.Drawing.Point(12, 141);
+			this.button_4.Name = "button_4";
+			this.button_4.Size = new System.Drawing.Size(59, 56);
+			this.button_4.TabIndex = 6;
+			this.button_4.Text = "4";
+			this.button_4.UseVisualStyleBackColor = true;
+			this.button_4.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_5
+			// 
+			this.button_5.Location = new System.Drawing.Point(77, 141);
+			this.button_5.Name = "button_5";
+			this.button_5.Size = new System.Drawing.Size(59, 56);
+			this.button_5.TabIndex = 7;
+			this.button_5.Text = "5";
+			this.button_5.UseVisualStyleBackColor = true;
+			this.button_5.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_6
+			// 
+			this.button_6.Location = new System.Drawing.Point(142, 141);
+			this.button_6.Name = "button_6";
+			this.button_6.Size = new System.Drawing.Size(59, 56);
+			this.button_6.TabIndex = 8;
+			this.button_6.Text = "6";
+			this.button_6.UseVisualStyleBackColor = true;
+			this.button_6.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_1
+			// 
+			this.button_1.Location = new System.Drawing.Point(12, 203);
+			this.button_1.Name = "button_1";
+			this.button_1.Size = new System.Drawing.Size(59, 56);
+			this.button_1.TabIndex = 9;
+			this.button_1.Text = "1";
+			this.button_1.UseVisualStyleBackColor = true;
+			this.button_1.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_2
+			// 
+			this.button_2.Location = new System.Drawing.Point(77, 203);
+			this.button_2.Name = "button_2";
+			this.button_2.Size = new System.Drawing.Size(59, 56);
+			this.button_2.TabIndex = 10;
+			this.button_2.Text = "2";
+			this.button_2.UseVisualStyleBackColor = true;
+			this.button_2.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_3
+			// 
+			this.button_3.Location = new System.Drawing.Point(142, 203);
+			this.button_3.Name = "button_3";
+			this.button_3.Size = new System.Drawing.Size(59, 56);
+			this.button_3.TabIndex = 11;
+			this.button_3.Text = "3";
+			this.button_3.UseVisualStyleBackColor = true;
+			this.button_3.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_0
+			// 
+			this.button_0.Location = new System.Drawing.Point(77, 265);
+			this.button_0.Name = "button_0";
+			this.button_0.Size = new System.Drawing.Size(59, 56);
+			this.button_0.TabIndex = 12;
+			this.button_0.Text = "0";
+			this.button_0.UseVisualStyleBackColor = true;
+			this.button_0.Click += new System.EventHandler(this.button_7_Click);
+			// 
+			// button_clear
+			// 
+			this.button_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+			this.button_clear.Location = new System.Drawing.Point(207, 141);
+			this.button_clear.Name = "button_clear";
+			this.button_clear.Size = new System.Drawing.Size(148, 56);
+			this.button_clear.TabIndex = 12;
+			this.button_clear.Text = "Clear";
+			this.button_clear.UseVisualStyleBackColor = true;
+			this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 14F);
+			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+			this.richTextBox1.Size = new System.Drawing.Size(343, 61);
+			this.richTextBox1.TabIndex = 13;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseClick);
+			this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(365, 329);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button_clear);
+			this.Controls.Add(this.button_0);
+			this.Controls.Add(this.button_3);
+			this.Controls.Add(this.button_2);
+			this.Controls.Add(this.button_1);
+			this.Controls.Add(this.button_6);
+			this.Controls.Add(this.button_5);
+			this.Controls.Add(this.button_4);
+			this.Controls.Add(this.button_9);
+			this.Controls.Add(this.button_8);
+			this.Controls.Add(this.button_7);
+			this.Controls.Add(this.button_factoriel);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button_factoriel;
+		private System.Windows.Forms.Button button_7;
+		private System.Windows.Forms.Button button_8;
+		private System.Windows.Forms.Button button_9;
+		private System.Windows.Forms.Button button_4;
+		private System.Windows.Forms.Button button_5;
+		private System.Windows.Forms.Button button_6;
+		private System.Windows.Forms.Button button_1;
+		private System.Windows.Forms.Button button_2;
+		private System.Windows.Forms.Button button_3;
+		private System.Windows.Forms.Button button_0;
+		private System.Windows.Forms.Button button_clear;
+		private System.Windows.Forms.RichTextBox richTextBox1;
 	}
 }
 
--- Class_factoriel/Form1.cs
+++ Class_factoriel/Form1.cs
@@ -12,9 +12,93 @@ namespace Class_factoriel
 {
 	public partial class Form1 : Form
 	{
+		bool is_result = false;
+		string SenderButton = string.Empty;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void button_factoriel_Click(object sender, EventArgs e)
+		{
+			is_result = true;
+			UInt64 InputNumber;
+
+			// ------------ Solotion 1
+			//byte InputNumber = byte.Parse(textBox_input.Text);
+
+			// ------------ Solotion 2
+			bool is_number = UInt64.TryParse(richTextBox1.Text, out InputNumber);
+			if (is_number == false)
+			{
+				richTextBox1.Text = $"Only numbers are valid !";
+			}
+			else
+			{
+				if (InputNumber > 22)
+				{
+					richTextBox1.Text = $"The result is too long !";
+				}
+				else
+				{
+					UInt64 result = 1;
+					for (UInt64 i = 1; i <= InputNumber; i++)
+					{
+						result = result * i;
+					}
+
+					string result_final = String.Format("{0:n0}", result);
+
+					richTextBox1.Text = string.Empty;
+					richTextBox1.SelectionFont = new Font("Microsoft YaHei UI Light", 10, FontStyle.Italic);
+					richTextBox1.SelectionColor = System.Drawing.Color.Gray;
+					richTextBox1.AppendText($"{InputNumber} factoriel ={Environment.NewLine}");
+					richTextBox1.SelectionFont = new Font("Microsoft YaHei UI Light", 14, FontStyle.Regular);
+					richTextBox1.AppendText($"{result_final}");
+				}
+			}
+
+
+		}
+
+
+		private void button_7_Click(object sender, EventArgs e)
+		{
+			if (is_result == true)
+			{
+				richTextBox1.Text = string.Empty;
+			}
+
+			is_result = false;
+
+			SenderButton = (sender as Button).Text;
+
+			richTextBox1.Text = $"{richTextBox1.Text}{SenderButton}";
+		}
+
+		private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (is_result == false)
+			{
+				SenderButton = e.KeyValue.ToString();
+			}
+		}
+
+		private void button_clear_Click(object sender, EventArgs e)
+		{
+			is_result = false;
+			richTextBox1.Text = string.Empty;
+		}
+
+
+		private void richTextBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (is_result == true)
+			{
+				richTextBox1.Text = string.Empty;
+			}
+			is_result = false;
 		}
 	}
 }
